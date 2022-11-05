8c4742d8b482ee087ef891f4574a006835d3aaa8
dravgust <dravgust@hotmail.com>
2022-11-05 17:37:05 +0200

    add Vayosoft.gRPC

---
+++ src/Vayosoft.gRPC/Reactive/AsyncStreamReaderObservableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Core;
+
+namespace Vayosoft.gRPC.Reactive;
+
+public static class AsyncStreamReaderObservableExtensions
+{
+    public static IObservable<T> AsObservable<T>(
+        this IAsyncStreamReader<T> reader,
+        CancellationToken cancellationToken = default) =>
+        new GrpcStreamObservable<T>(reader, cancellationToken);
+}
