188063db05d3c61ed4047740f9459a50eb9bf1e0
anton <anton@vayosoft.com>
2022-11-16 12:23:49 +0200

    linq2db

---
+++ src/Vayosoft.Linq2db.MySQL/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using LinqToDB.AspNet;
+using LinqToDB.AspNet.Logging;
+using LinqToDB.Configuration;
+using LinqToDB.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Vayosoft.Linq2db.MySQL
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddMySqlContext<TContext>(this IServiceCollection services, IConfiguration configuration) where TContext : DataConnection
+        {
+            var connectionString = configuration["Linq2DbContext:ConnectionString"]
+                                   ?? configuration.GetConnectionString("DefaultConnection")!;
+
+            services.AddLinqToDBContext<TContext>((provider, options) =>
+            {
+                options
+                    //will configure the AppDataConnection to use
+                    //SqlServer with the provided connection string
+                    //there are methods for each supported database
+                    .UseSqlServer(connectionString)
+
+                    //default logging will log everything using
+                    //an ILoggerFactory configured in the provider
+                    .UseDefaultLogging(provider);
+            });
+
+
+            return services;
+        }
+    }
+}
---
+++ src/Vayosoft.Linq2db.MySQL/DataContext.cs
@@ -0,0 +1,72 @@
+﻿using LinqToDB;
+using LinqToDB.Configuration;
+using LinqToDB.Data;
+using Vayosoft.Commons.Entities;
+using Vayosoft.Commons.Exceptions;
+using Vayosoft.Persistence;
+using Vayosoft.Specifications;
+
+
+namespace Vayosoft.Linq2db.MySQL
+{
+    public class DataContext : DataConnection, ILinqProvider, IUnitOfWork
+    {
+        public DataContext(LinqToDBConnectionOptions<DataContext> options)
+            : base(options)
+        { }
+
+        public IQueryable<TEntity> AsQueryable<TEntity>() where TEntity : class, IEntity {
+            return this.GetTable<TEntity>().AsQueryable();
+        }
+
+        public IQueryable<TEntity> AsQueryable<TEntity>(ISpecification<TEntity> specification) where TEntity : class, IEntity {
+            return new SpecificationEvaluator<TEntity>().Evaluate(AsQueryable<TEntity>(), specification);
+        }
+
+        public void Add<TEntity>(TEntity entity) where TEntity : class, IEntity
+        {
+           this.GetTable<TEntity>().Insert(() => entity);
+        }
+
+        public void Update<TEntity>(TEntity entity) where TEntity : class, IEntity
+        {
+            this.GetTable<TEntity>().Update(obj => entity);
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : class, IEntity
+        {
+            this.GetTable<TEntity>().Delete(obj => obj.Id == entity.Id);
+        }
+
+        public TEntity Find<TEntity>(object id) where TEntity : class, IEntity
+        {
+            return this.GetTable<TEntity>().SingleOrDefault(x => x.Id == id);
+        }
+
+        public TEntity Get<TEntity>(object id) where TEntity : class, IEntity
+        {
+            var entity = Find<TEntity>(id);
+            if (entity == null)
+                throw EntityNotFoundException.For<TEntity>(id);
+
+            return entity;
+        }
+
+        public Task<TEntity> FindAsync<TEntity>(object id, CancellationToken cancellationToken) where TEntity : class, IEntity
+        {
+            return this.GetTable<TEntity>().FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+
+        public async Task<TEntity> GetAsync<TEntity>(object id, CancellationToken cancellationToken) where TEntity : class, IEntity
+        {
+            var entity = await FindAsync<TEntity>(id, cancellationToken);
+            if (entity == null)
+                throw EntityNotFoundException.For<TEntity>(id);
+
+            return entity;
+        }
+
+        public void Commit() => this.CommitTransaction();
+        public Task CommitAsync() => this.CommitTransactionAsync();
+    }
+}
