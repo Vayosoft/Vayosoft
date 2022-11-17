87646142b0aeed477d47f4bbccc127f6d7e6e10d
dravgust <dravgust@hotmail.com>
2022-11-17 21:21:14 +0200

    persistence

--- src/Vayosoft/Persistence/Specifications/ILinqSpecification.cs
+++ src/Vayosoft/Persistence/Specifications/ILinqSpecification.cs
@@ -1,12 +1,8 @@
-﻿using System.Linq.Expressions;
-
-namespace Vayosoft.Persistence.Specifications
+﻿namespace Vayosoft.Persistence.Specifications
 {
     public interface ILinqSpecification<T>
         where T : class
     {
-        IReadOnlyCollection<Expression<Func<T, object>>> Includes { get; }
-
         IQueryable<T> Apply(IQueryable<T> query);
     }
 }
