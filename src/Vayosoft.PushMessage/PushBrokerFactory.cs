362b93e4471ea8eed9af4a5061ac206d8c09748d
anton <anton@vayosoft.com>
2022-11-27 13:10:47 +0200

    .

--- src/Vayosoft.PushMessage/PushBrokerFactory.cs
+++ src/Vayosoft.PushMessage/PushBrokerFactory.cs
@@ -6,18 +6,18 @@ namespace Vayosoft.PushMessage
 
     public class PushBrokerFactory
     {
-        private readonly IServiceProvider serviceProvider;
+        private readonly IServiceProvider _serviceProvider;
         public PushBrokerFactory(IServiceProvider serviceProvider)
         {
-            this.serviceProvider = serviceProvider;
+            this._serviceProvider = serviceProvider;
         }
 
         public IPushBroker GetFor(string platformName)
         {
             return platformName switch
             {
-                "IOS" => (IPushBroker) serviceProvider.GetService(typeof(ApplePushBroker))!,
-                "Android" => (IPushBroker) serviceProvider.GetService(typeof(GooglePushBroker))!,
+                "IOS" => (IPushBroker) _serviceProvider.GetService(typeof(ApplePushBroker))!,
+                "Android" => (IPushBroker) _serviceProvider.GetService(typeof(GooglePushBroker))!,
                 _ => throw new ArgumentOutOfRangeException(nameof(platformName), platformName, null)
             };
         }
