4a795cb15977388239ba89273229624069912370
anton <anton@vayosoft.com>
2023-02-02 13:30:02 +0200

    .

--- src/Vayosoft.Persistence.MongoDB/Extensions/PagingExtensions.cs
+++ src/Vayosoft.Persistence.MongoDB/Extensions/PagingExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using Vayosoft.Commons;
 using Vayosoft.Commons.Models.Pagination;
 
 namespace Vayosoft.Persistence.MongoDB.Extensions
@@ -7,28 +8,39 @@ namespace Vayosoft.Persistence.MongoDB.Extensions
     public static class PagingExtensions
     {
         public static IMongoQueryable<T> Paginate<T>(this IMongoQueryable<T> queryable, IPagingModel pagingModel)
-            where T : class
             => Paginate(queryable, pagingModel.Page, pagingModel.PageSize);
 
         public static IMongoQueryable<T> Paginate<T>(this IMongoQueryable<T> queryable, int page, int pageSize)
-            where T : class
             => queryable.Skip((page - 1) * pageSize).Take(pageSize);
 
         public static Task<IPagedEnumerable<T>> ToPagedEnumerableAsync<T>(this IMongoQueryable<T> queryable,
             IPagingModel pagingModel, CancellationToken cancellationToken = default)
-            where T : class
         {
             return queryable.ToPagedEnumerableAsync(pagingModel.Page, pagingModel.PageSize, cancellationToken);
         }
 
         public static async Task<IPagedEnumerable<T>> ToPagedEnumerableAsync<T>(this IMongoQueryable<T> queryable,
             int page = 1, int pageSize = IPagingModel.DefaultSize, CancellationToken cancellationToken = default)
-            where T : class
         {
             var list = queryable.Paginate(page, pageSize).ToListAsync(cancellationToken: cancellationToken);
             var count = queryable.CountAsync(cancellationToken: cancellationToken);
             await Task.WhenAll(list, count);
             return new PagedCollection<T>(await list, await count);
+        }
+
+        public static IFindFluent<TDocument, TProjection> Paginate<TDocument, TProjection>(this IFindFluent<TDocument, TProjection> query,
+            IPagingModel pagingModel) => Paginate(query, pagingModel.Page, pagingModel.PageSize);
+
+        public static IFindFluent<TDocument, TProjection> Paginate<TDocument, TProjection>(this IFindFluent<TDocument, TProjection> query,
+            int page, int pageSize) => query.Skip((page - 1) * pageSize).Limit(pageSize);
+
+        public static async Task<IPagedEnumerable<TProjection>> ToPagedEnumerableAsync<TDocument, TProjection>(this IFindFluent<TDocument, TProjection> query,
+            int page = 1, int pageSize = IPagingModel.DefaultSize, CancellationToken cancellationToken = default)
+        {
+            var list = query.Paginate(page, pageSize).ToListAsync(cancellationToken);
+            var count = query.CountDocumentsAsync(cancellationToken: cancellationToken);
+            await Task.WhenAll(list, count);
+            return new PagedCollection<TProjection>(await list, await count);
         }
     }
 }
